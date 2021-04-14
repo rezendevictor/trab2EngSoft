1f6a27f4cb94f5b879f1d5a680a6908ec1edbbf7
Victor <victorrzn13@gmail.com>
2021-04-13 23:54:50 -0300

    Comentarios

--- lib/controller/home.controller.dart
+++ lib/controller/home.controller.dart
@@ -6,10 +6,13 @@ class HomeController extends BaseController {
 
 
   List<String> reviews = [
-    "Recomendo! Muito muito satisfeita após preenchimento e botox. (…) fui aconselhada por uma amiga que tinha feito harmonização facial com resultados muito bons. O Dr. Heartfild é um verdadeiro profissional, dedicado e super simpático. Tratamento personalizado, com resposta a todas as dúvidas por telefone durante o tratamento o que me deu muita tranquilidade, (…) Clínica super acolhedora e atendimento top.",
+    "Recomendo! Muito muito satisfeita após preenchimento e botox. (…) fui aconselhada por uma amiga que tinha feito harmonização facial com resultados muito bons.",
     "Ambiente super acolhedor. Equipa super simpática 🙂 Recomendo",
     " Recomendo, além de bons profissionais, têm bom atendimento. Obrigada.",
-    "O Terapeuta Tiago foi uma preciosa ajuda para a minha Leonor. Só lamento não o ter conhecido mais cedo. Muito agradeço o profissionalismo e simpatia."
+    "O Terapeuta Tiago foi uma preciosa ajuda para a minha Leonor. Só lamento não o ter conhecido mais cedo. Muito agradeço o profissionalismo e simpatia.",
+"  Excelente profissional ! Fiz alguns procedimentos no rosto, foi super tranquilo e ninguém consegue ver a olho nú, se eu não contar ninguém sabe ",
+    "Fiquei muito feliz por minha escolha - Dr. Newton (sem palavras)...",
+    "Dr.Newton,  mãos abençoadas, educado, gentil, muito atencioso, adorei conhece.",
   ];
 
   String rand_say(){
